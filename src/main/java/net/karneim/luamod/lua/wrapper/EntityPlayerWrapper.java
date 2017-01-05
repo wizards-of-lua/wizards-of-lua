bf9cdbdcbd2dafe8318cab7e9844c1fa23618b56
Michael Karneim <mkarneim@googlemail.com>
2017-01-05 23:02:41 +0100

    Support in Lua: player.getInventory() and itemstack.getData()

--- src/main/java/net/karneim/luamod/lua/wrapper/EntityPlayerWrapper.java
+++ src/main/java/net/karneim/luamod/lua/wrapper/EntityPlayerWrapper.java
@@ -4,8 +4,13 @@ import javax.annotation.Nullable;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.GameType;
 import net.sandius.rembulan.impl.ImmutableTable;
+import net.sandius.rembulan.impl.NonsuspendableFunctionException;
+import net.sandius.rembulan.runtime.AbstractFunction1;
+import net.sandius.rembulan.runtime.ExecutionContext;
+import net.sandius.rembulan.runtime.ResolvedControlThrowable;
 
 public class EntityPlayerWrapper extends EntityLivingBaseWrapper<EntityPlayer> {
   public EntityPlayerWrapper(@Nullable EntityPlayer delegate) {
@@ -24,7 +29,36 @@ public class EntityPlayerWrapper extends EntityLivingBaseWrapper<EntityPlayer> {
       EntityPlayerMP mp = (EntityPlayerMP) delegate;
       GameType e = mp.interactionManager.getGameType();
       builder.add("gamemode", new EnumWrapper(e).getLuaObject());
+      builder.add("getInventory", new GetInventoryFunction(mp));
     }
   }
 
+  private class GetInventoryFunction extends AbstractFunction1 {
+    private EntityPlayerMP mp;
+
+    GetInventoryFunction(EntityPlayerMP mp) {
+      this.mp = mp;
+    }
+
+    @Override
+    public void invoke(ExecutionContext context, Object arg1) throws ResolvedControlThrowable {
+      if (arg1 == null) {
+        throw new IllegalArgumentException(String.format("number expected but got nil!"));
+      }
+      if (!(arg1 instanceof Number)) {
+        throw new IllegalArgumentException(
+            String.format("number expected but got %s", arg1.getClass().getSimpleName()));
+      }
+      int index = ((Number) (arg1)).intValue();
+      ItemStack itemStack = mp.inventory.getStackInSlot(index);
+      ItemStackWrapper wrapper = new ItemStackWrapper(itemStack);
+      context.getReturnBuffer().setTo(wrapper.getLuaObject());
+    }
+
+    @Override
+    public void resume(ExecutionContext context, Object suspendedState)
+        throws ResolvedControlThrowable {
+      throw new NonsuspendableFunctionException();
+    }
+  }
 }
--- src/main/java/net/karneim/luamod/lua/wrapper/ItemStackWrapper.java
+++ src/main/java/net/karneim/luamod/lua/wrapper/ItemStackWrapper.java
@@ -2,10 +2,20 @@ package net.karneim.luamod.lua.wrapper;
 
 import javax.annotation.Nullable;
 
+import net.karneim.luamod.LuaMod;
+import net.karneim.luamod.lua.NBTTagUtil;
+import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 import net.sandius.rembulan.impl.ImmutableTable;
+import net.sandius.rembulan.impl.NonsuspendableFunctionException;
+import net.sandius.rembulan.runtime.AbstractFunction0;
+import net.sandius.rembulan.runtime.AbstractFunction3;
+import net.sandius.rembulan.runtime.ExecutionContext;
+import net.sandius.rembulan.runtime.ResolvedControlThrowable;
 
 public class ItemStackWrapper extends StructuredLuaWrapper<ItemStack> {
   public ItemStackWrapper(@Nullable ItemStack delegate) {
@@ -18,6 +28,8 @@ public class ItemStackWrapper extends StructuredLuaWrapper<ItemStack> {
     builder.add("displayName", delegate.getDisplayName());
     builder.add("damage", delegate.getItemDamage());
     builder.add("name", getName());
+    builder.add("spawn", new SpawnFunction());
+    builder.add("getData", new GetDataFunction());
   }
 
   private String getName() {
@@ -27,4 +39,62 @@ public class ItemStackWrapper extends StructuredLuaWrapper<ItemStack> {
     ResourceLocation registryName = item.getRegistryName();
     return registryName.getResourceDomain() + ":" + registryName.getResourcePath();
   }
+
+  private class GetDataFunction extends AbstractFunction0 {
+
+    @Override
+    public void invoke(ExecutionContext context) throws ResolvedControlThrowable {
+      NBTTagCompound tagCompound = delegate.getTagCompound();
+      ImmutableTable.Builder builder = new ImmutableTable.Builder();
+      NBTTagUtil.insertValues(builder, tagCompound);
+      ImmutableTable tbl = builder.build();
+
+      context.getReturnBuffer().setTo(tbl);
+    }
+
+    @Override
+    public void resume(ExecutionContext context, Object suspendedState)
+        throws ResolvedControlThrowable {
+      throw new NonsuspendableFunctionException();
+    }
+  }
+  
+  private class SpawnFunction extends AbstractFunction3 {
+
+    @Override
+    public void invoke(ExecutionContext context, Object arg1, Object arg2, Object arg3)
+        throws ResolvedControlThrowable {
+      if (arg1 == null || !(arg1 instanceof Number)) {
+        throw new IllegalArgumentException(
+            String.format("Integer value for dx expected but got %s!", arg1));
+      }
+      if (arg2 == null || !(arg2 instanceof Number)) {
+        throw new IllegalArgumentException(
+            String.format("Integer value for dy expected but got %s!", arg2));
+      }
+      if (arg3 == null || !(arg3 instanceof Number)) {
+        throw new IllegalArgumentException(
+            String.format("Integer value for dz expected but got %s!", arg3));
+      }
+
+      double x = ((Number) arg1).doubleValue();
+      double y = ((Number) arg2).doubleValue();
+      double z = ((Number) arg3).doubleValue();
+
+      // TODO this is just the overworld. This won't work in the Nether. What to do?
+      World world = LuaMod.instance.getServer().getEntityWorld();
+
+      InventoryHelper.spawnItemStack(world, x, y, z, delegate);
+
+      context.getReturnBuffer().setTo();
+    }
+
+    @Override
+    public void resume(ExecutionContext context, Object suspendedState)
+        throws ResolvedControlThrowable {
+      throw new NonsuspendableFunctionException();
+    }
+  }
+
+  //
 }
