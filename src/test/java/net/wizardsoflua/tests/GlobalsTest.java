6f47ce73a30d148a6ec1a518c65feec95566bedf
Michael Karneim <mkarneim@googlemail.com>
2017-08-06 12:06:22 +0200

    GlobalsTest

--- src/test/java/net/wizardsoflua/tests/GlobalsTest.java
+++ src/test/java/net/wizardsoflua/tests/GlobalsTest.java
@@ -1,66 +1,42 @@
-//package net.wizardsoflua.tests;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//
-//import net.wizardsoflua.testenv.MinecraftJUnitRunner;
-//import net.wizardsoflua.testenv.WolTestBase;
-//import net.wizardsoflua.testenv.event.ServerLog4jEvent;
-//
-//@RunWith(MinecraftJUnitRunner.class)
-//public class GlobalsTest extends WolTestBase {
-//
-//  // /test net.wizardsoflua.tests.GlobalsTest test_check_number_with_number
-//  @Test
-//  public void test_check_number_with_number() throws Exception {
-//    // Given:
-//
-//    // When:
-//    mc().executeCommand("/lua check.number(1); print('ok')");
-//
-//    // Then:
-//    ServerLog4jEvent act = mc().waitFor(ServerLog4jEvent.class);
-//    assertThat(act.getMessage()).isEqualTo("ok");
-//  }
-//
-//  // /test net.wizardsoflua.tests.GlobalsTest test_check_number_with_not_a_number
-//  @Test
-//  public void test_check_number_with_not_a_number() throws Exception {
-//    // Given:
-//
-//    // When:
-//    mc().executeCommand("/lua check.number('xxx')");
-//
-//    // Then:
-//    ServerLog4jEvent act = mc().waitFor(ServerLog4jEvent.class);
-//    assertThat(act.getMessage()).contains("number expected");
-//  }
-//
-//  // /test net.wizardsoflua.tests.GlobalsTest test_check_string_with_string
-//  @Test
-//  public void test_check_string_with_string() throws Exception {
-//    // Given:
-//
-//    // When:
-//    mc().executeCommand("/lua check.string('abc'); print('ok')");
-//
-//    // Then:
-//    ServerLog4jEvent act = mc().waitFor(ServerLog4jEvent.class);
-//    assertThat(act.getMessage()).isEqualTo("ok");
-//  }
-//
-//  // /test net.wizardsoflua.tests.GlobalsTest test_check_string_with_not_a_string
-//  @Test
-//  public void test_check_string_with_not_a_string() throws Exception {
-//    // Given:
-//
-//    // When:
-//    mc().executeCommand("/lua check.string({})");
-//
-//    // Then:
-//    ServerLog4jEvent act = mc().waitFor(ServerLog4jEvent.class);
-//    assertThat(act.getMessage()).contains("string expected");
-//  }
-//
-//
-//}
+package net.wizardsoflua.tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import net.wizardsoflua.testenv.MinecraftJUnitRunner;
+import net.wizardsoflua.testenv.WolTestBase;
+import net.wizardsoflua.testenv.event.ServerLog4jEvent;
+
+@RunWith(MinecraftJUnitRunner.class)
+public class GlobalsTest extends WolTestBase {
+
+  // /test net.wizardsoflua.tests.GlobalsTest test_str
+  @Test
+  public void test_str() throws Exception {
+    // Given:
+
+    // When:
+    mc().executeCommand("/lua t={a=1,b='hello'}; print(str(t))");
+
+    // Then:
+    ServerLog4jEvent act = mc().waitFor(ServerLog4jEvent.class);
+    assertThat(act.getMessage()).isEqualTo("{\n  [\"a\"] = 1,\n  [\"b\"] = \"hello\"\n}");
+  }
+
+  // /test net.wizardsoflua.tests.GlobalsTest test_sleep
+  @Test
+  public void test_sleep() throws Exception {
+    // Given:
+    long sleepTime = 10;
+    // When:
+    mc().executeCommand(
+        "/lua print(Runtime.getGametime()); sleep(%s); print(Runtime.getGametime())", sleepTime);
+
+    // Then:
+    ServerLog4jEvent message1 = mc().waitFor(ServerLog4jEvent.class);
+    ServerLog4jEvent message2 = mc().waitFor(ServerLog4jEvent.class);
+    long actual = Long.parseLong(message2.getMessage()) - Long.parseLong(message1.getMessage());
+    assertThat(actual).isEqualTo(sleepTime);
+  }
+
+}
