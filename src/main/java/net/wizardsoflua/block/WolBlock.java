5cfa5600d83609019eb1a022636004779558c411
Michael Karneim <mkarneim@googlemail.com>
2017-08-24 17:37:23 +0200

    WolBlock.setBlock(): clear any inventory block before replacing it

--- src/main/java/net/wizardsoflua/block/WolBlock.java
+++ src/main/java/net/wizardsoflua/block/WolBlock.java
@@ -3,6 +3,7 @@ package net.wizardsoflua.block;
 import javax.annotation.Nullable;
 
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
@@ -41,25 +42,32 @@ public class WolBlock {
   }
 
   public void setBlock(World world, BlockPos pos) {
-    world.setBlockState(pos, blockState);
-    if (nbt != null) {
-      // TODO remove this side effect
-      // however, it does not hurt
-      nbt.setInteger("x", pos.getX());
-      nbt.setInteger("y", pos.getY());
-      nbt.setInteger("z", pos.getZ());
-
-      TileEntity tileEntity = world.getTileEntity(pos);
-      if (tileEntity != null) {
-        tileEntity.readFromNBT(nbt);
-        tileEntity.markDirty();
-      } else {
-        throw new IllegalStateException(String.format("Missing tile entity for %s at %s %s %s",
-            blockState.getBlock().getRegistryName().getResourcePath(), pos.getX(), pos.getY(),
-            pos.getZ()));
+    TileEntity oldTileEntity = world.getTileEntity(pos);
+    if (oldTileEntity != null) {
+      if (oldTileEntity instanceof IInventory) {
+        ((IInventory) oldTileEntity).clear();
       }
-      int flags = 3; // TODO why 3?
-      world.notifyBlockUpdate(pos, blockState, blockState, flags);
+    }
+    if (world.setBlockState(pos, blockState)) {
+      if (nbt != null) {
+        // TODO remove this side effect
+        // however, it does not hurt
+        nbt.setInteger("x", pos.getX());
+        nbt.setInteger("y", pos.getY());
+        nbt.setInteger("z", pos.getZ());
+
+        TileEntity tileEntity = world.getTileEntity(pos);
+        if (tileEntity != null) {
+          tileEntity.readFromNBT(nbt);
+          tileEntity.markDirty();
+        } else {
+          throw new IllegalStateException(String.format("Missing tile entity for %s at %s %s %s",
+              blockState.getBlock().getRegistryName().getResourcePath(), pos.getX(), pos.getY(),
+              pos.getZ()));
+        }
+        int flags = 3; // Do a block update (1) and send it to all clients (2)
+        world.notifyBlockUpdate(pos, blockState, blockState, flags);
+      }
     }
   }
 }
