b86d7e20daffacd351fc2d612648a01baf598623
Adrodoc55 <adrodoc55@googlemail.com>
2018-02-17 22:03:59 +0100

    Fix player duplication bug

--- src/main/java/net/wizardsoflua/lua/classes/common/LuaInstanceProxy.java
+++ src/main/java/net/wizardsoflua/lua/classes/common/LuaInstanceProxy.java
@@ -6,12 +6,10 @@ import net.wizardsoflua.lua.classes.ProxyingLuaClass;
 
 public abstract class LuaInstanceProxy<D> extends DelegatingProxy<D> {
   protected final ProxyingLuaClass<?, ?> luaClass;
-  protected D delegate;
 
   public LuaInstanceProxy(ProxyingLuaClass<?, ?> luaClass, D delegate) {
     super(luaClass.getClassLoader(), luaClass.getMetaTable(), delegate);
     this.luaClass = checkNotNull(luaClass, "luaClass == null!");
-    this.delegate = checkNotNull(delegate, "delegate==null!");
   }
 
   public ProxyingLuaClass<?, ?> getLuaClass() {
