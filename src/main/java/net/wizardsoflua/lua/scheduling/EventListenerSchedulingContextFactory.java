fcd6099e13d33fad1589ff42e7bee1c49fe5ead5
Adrodoc55 <adrodoc55@googlemail.com>
2018-03-10 00:52:41 +0100

    Trying to cancel an event outside of an event listener is an illegal operation

--- src/main/java/net/wizardsoflua/lua/scheduling/EventListenerSchedulingContextFactory.java
+++ src/main/java/net/wizardsoflua/lua/scheduling/EventListenerSchedulingContextFactory.java
@@ -1,7 +1,10 @@
 package net.wizardsoflua.lua.scheduling;
 
+import static net.wizardsoflua.lua.scheduling.LuaExecutor.Type.EVENT_LISTENER;
+
 import net.sandius.rembulan.runtime.IllegalOperationAttemptException;
 import net.sandius.rembulan.runtime.SchedulingContext;
+import net.wizardsoflua.lua.scheduling.LuaExecutor.Type;
 
 public class EventListenerSchedulingContextFactory implements LuaSchedulingContextFactory {
   private int luaTickLimit;
@@ -36,6 +39,11 @@ public class EventListenerSchedulingContextFactory implements LuaSchedulingConte
       @Override
       public void setAutosleep(boolean autosleep) {
         throw new IllegalOperationAttemptException("attempt to set autosleep");
+      }
+
+      @Override
+      public Type getLuaExecutorType() {
+        return EVENT_LISTENER;
       }
     };
   }
