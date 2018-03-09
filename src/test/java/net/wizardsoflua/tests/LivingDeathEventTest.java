175e145c5ad1b3e82ddae3f6252389202f7d0f8f
Michael Karneim <mkarneim@googlemail.com>
2018-03-09 11:41:08 +0100

    Fix failing LivingDeathEventTest

--- src/test/java/net/wizardsoflua/tests/LivingDeathEventTest.java
+++ src/test/java/net/wizardsoflua/tests/LivingDeathEventTest.java
@@ -21,8 +21,7 @@ public class LivingDeathEventTest extends WolTestBase {
     mc().clearEvents();
 
     // When:
-    mc().executeCommand(
-        "/lua q=Events.connect('LivingDeathEvent'); e=q:next(); print(e.cause)");
+    mc().executeCommand("/lua q=Events.connect('LivingDeathEvent'); e=q:next(); print(e.cause)");
     mc().executeCommand("/kill @e[type=pig,name=testpig]");
 
     // Then:
@@ -57,20 +56,21 @@ public class LivingDeathEventTest extends WolTestBase {
   @Test
   public void test_entity_is_player() {
     // Given:
+    String playerName = mc().player().getName();
 
     // When:
     try {
       mc().executeCommand(
           "/lua q=Events.connect('LivingDeathEvent'); e=q:next(); print(e.entity.name)");
-      mc().executeCommand("/kill @a[name=testplayer]");
+      mc().executeCommand("/kill @a[name=%s]", playerName);
 
       // Then:
       ServerLog4jEvent act1 = mc().waitFor(ServerLog4jEvent.class);
-      assertThat(act1.getMessage()).startsWith("testplayer fell out of the world");
+      assertThat(act1.getMessage()).startsWith(playerName + " fell out of the world");
       ServerLog4jEvent act2 = mc().waitFor(ServerLog4jEvent.class);
-      assertThat(act2.getMessage()).startsWith("Killed testplayer");
+      assertThat(act2.getMessage()).startsWith("Killed " + playerName);
       ServerLog4jEvent act3 = mc().waitFor(ServerLog4jEvent.class);
-      assertThat(act3.getMessage()).isEqualTo("testplayer");
+      assertThat(act3.getMessage()).isEqualTo(playerName);
 
     } finally {
       mc().player().respawn();
