c85f4999ccd665e11d74850cdb0f53ffa70d7954
Adrodoc55 <adrodoc55@googlemail.com>
2018-03-10 19:17:04 +0100

    Switch to generated DroppedItemClass

--- src/main/java/net/wizardsoflua/lua/classes/entity/DroppedItemApi.java
+++ src/main/java/net/wizardsoflua/lua/classes/entity/DroppedItemApi.java
@@ -7,15 +7,22 @@ import net.wizardsoflua.annotation.GenerateLuaDoc;
 import net.wizardsoflua.annotation.LuaProperty;
 import net.wizardsoflua.lua.classes.ProxyingLuaClass;
 
+/**
+ * The <span class="notranslate">DroppedItem</span> class represents things that are lying somewhere
+ * and can be collected by players.
+ */
 @GenerateLuaClass(name = DroppedItemApi.NAME)
 @GenerateLuaDoc(subtitle = "Things That are Lying Around")
 public class DroppedItemApi<D extends EntityItem> extends EntityApi<D> {
-  public static final String NAME = "DroppedItem2";
+  public static final String NAME = "DroppedItem";
 
   public DroppedItemApi(ProxyingLuaClass<?, ?> luaClass, D delegate) {
     super(luaClass, delegate);
   }
 
+  /**
+   * This is the [item](/modules/Item/) that has been dropped.
+   */
   @LuaProperty
   public ItemStack getItem() {
     return delegate.getEntityItem();
