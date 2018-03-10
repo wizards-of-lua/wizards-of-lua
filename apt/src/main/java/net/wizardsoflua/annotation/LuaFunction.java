1d93ae739e335bfcdd3d1db0894af294824396cb
Adrodoc55 <adrodoc55@googlemail.com>
2018-03-10 16:38:52 +0100

    Refactor annotation retentions

--- apt/src/main/java/net/wizardsoflua/annotation/LuaFunction.java
+++ apt/src/main/java/net/wizardsoflua/annotation/LuaFunction.java
@@ -2,12 +2,12 @@ package net.wizardsoflua.annotation;
 
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.SOURCE)
+@Retention(SOURCE)
 @Target({METHOD, TYPE})
 public @interface LuaFunction {
 }
--- apt/src/main/java/net/wizardsoflua/annotation/LuaModuleDoc.java
+++ apt/src/main/java/net/wizardsoflua/annotation/LuaModuleDoc.java
@@ -1,12 +1,12 @@
 package net.wizardsoflua.annotation;
 
 import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.SOURCE)
+@Retention(SOURCE)
 @Target(TYPE)
 public @interface LuaModuleDoc {
   /**
