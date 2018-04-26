53321432ce90f00ae37aaa6a4ef77ba09df5b556
Michael Karneim <mkarneim@googlemail.com>
2018-04-26 21:20:47 +0200

    BlockApi

---
+++ src/main/java/net/wizardsoflua/lua/classes/block/BlockStateConverter.java
@@ -0,0 +1,43 @@
+package net.wizardsoflua.lua.classes.block;
+
+import java.util.Collection;
+
+import com.google.auto.service.AutoService;
+
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.IBlockState;
+import net.sandius.rembulan.Table;
+import net.wizardsoflua.extension.spell.spi.LuaConverter;
+import net.wizardsoflua.lua.table.PatchedImmutableTable;
+
+@AutoService(LuaConverter.class)
+public class BlockStateConverter implements LuaConverter<WolBlockState, Table> {
+
+  @Override
+  public Class<WolBlockState> getJavaClass() {
+    return WolBlockState.class;
+  }
+
+  @Override
+  public Class<Table> getLuaClass() {
+    return Table.class;
+  }
+
+  @Override
+  public WolBlockState getJavaInstance(Table luaInstance) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Table getLuaInstance(WolBlockState blockState) {
+    PatchedImmutableTable.Builder b = new PatchedImmutableTable.Builder();
+    Collection<IProperty<?>> names = blockState.getDelegate().getPropertyKeys();
+    for (IProperty<?> name : names) {
+      Comparable<?> value = blockState.getDelegate().getValue(name);
+      Object luaValue = BlockPropertyConverter.toLua(value);
+      b.add(name.getName(), luaValue);
+    }
+    return b.build();
+  }
+
+}
---
+++ src/main/java/net/wizardsoflua/lua/classes/block/WolBlockState.java
@@ -0,0 +1,16 @@
+package net.wizardsoflua.lua.classes.block;
+
+import net.minecraft.block.state.IBlockState;
+
+public class WolBlockState {
+  private IBlockState delegate;
+
+  public WolBlockState(IBlockState delegate) {
+    this.delegate = delegate;
+  }
+
+  public IBlockState getDelegate() {
+    return delegate;
+  }
+
+}
