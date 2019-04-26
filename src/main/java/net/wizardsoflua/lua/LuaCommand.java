27ffe8f1993f4efb02144b9758186cc01780c234
Adrodoc <adrodoc55@googlemail.com>
2019-04-26 14:11:09 +0200

    Remove custom ArgumentTypes because they are not sent to vanilla clients

--- src/main/java/net/wizardsoflua/lua/LuaCommand.java
+++ src/main/java/net/wizardsoflua/lua/LuaCommand.java
@@ -4,16 +4,13 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import static com.mojang.brigadier.arguments.StringArgumentType.greedyString;
 import static net.minecraft.command.Commands.argument;
 import static net.minecraft.command.Commands.literal;
-
 import java.io.PrintWriter;
 import java.io.StringWriter;
-
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-
 import net.minecraft.command.CommandSource;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentString;
@@ -39,7 +36,6 @@ public class LuaCommand implements Command<CommandSource> {
         literal("lua")//
             .then(argument("code", greedyString())//
                 .executes(this)));
-
   }
 
   @Override
