c33673e9125c3b400fb1d826b0133438f64b268d
Adrodoc <adrodoc55@googlemail.com>
2019-04-28 17:56:01 +0200

    Add @PreDestroy

--- src/main/java/net/wizardsoflua/lua/extension/ClassIndex.java
+++ src/main/java/net/wizardsoflua/lua/extension/ClassIndex.java
@@ -1,12 +1,13 @@
 package net.wizardsoflua.lua.extension;
 
-import java.util.HashMap;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.Map;
-
 import javax.annotation.Nullable;
 
 public class ClassIndex {
-  private final Map<Class<?>, Object> map = new HashMap<>();
+  private final Map<Class<?>, Object> map = new LinkedHashMap<>();
 
   public void add(Object element) {
     Class<?> cls = element.getClass();
@@ -16,5 +17,9 @@ public class ClassIndex {
   public @Nullable <E> E get(Class<E> cls) {
     Object value = map.get(cls);
     return cls.cast(value);
+  }
+
+  public Collection<Object> values() {
+    return Collections.unmodifiableCollection(map.values());
   }
 }
