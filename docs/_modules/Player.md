b5412248b290c8ad4a1fcba370ca78ad24fd5d08
Michael Karneim <mkarneim@googlemail.com>
2019-05-03 18:16:36 +0200

    Update docs for Player

--- docs/_modules/Player.md
+++ docs/_modules/Player.md
@@ -1,64 +1,75 @@
 ---
 name: Player
+title: Player
 subtitle: Controlling the Player
 type: class
 extends: Entity
 layout: module
 properties:
-  - name: gamemode
-    type: string
+  - name: 'gamemode'
+    type: 'string'
     access: r/w
     description: |
-        This is this player's game mode. It can be one of 'survival', 'adventure', 'creative', 'spectator'.
+        This is this player's game mode. It can be one of 'survival', 'adventure', 'creative',
+        'spectator'.
+       
         #### Example
+       
         Setting the gamemode of this spell's owner to 'adventure'.
+       
         ```lua
         spell.owner.gamemode = "adventure"
         ```
-  - name: health
-    type: number
+  - name: 'health'
+    type: 'number (float)'
     access: r/w
     description: |
-      The 'health' is the energy of this entity. When it falls to zero this entity dies.
-  - name: mainhand
-    type: "[Item](/modules/Item/)"
+        The 'health' is the energy of this entity. When it falls to zero this entity dies.
+  - name: 'mainhand'
+    type: '[Item](/modules/Item)'
     access: r/w
-    description: "This is the [item](/modules/Item/) this player is holding in his main hand.    
-    "  
-  - name: offhand
-    type: "[Item](/modules/Item/)"
+    description: |
+        This is the [item](/modules/Item) this entity is holding in its main hand.
+  - name: 'offhand'
+    type: '[Item](/modules/Item)'
     access: r/w
-    description: "This is the [item](/modules/Item/) this player is holding in his off hand.    
-    "
-  - name: operator
+    description: |
+        This is the [item](/modules/Item) this entity is holding in his off hand.
+  - name: 'operator'
     type: 'boolean'
     access: r
     description: |
-        This is true if this player has operator privileges.  
-  - name: team
-    type: string
+        This is <span class="notranslate">true</span> if this player has operator privileges.
+  - name: 'team'
+    type: 'string'
     access: r/w
     description: |
-        The 'team' is the name of the team this player belongs to, or nil if he is not a member of any team.
-
+        The 'team' is the name of the team this player belongs to, or
+        <span class="notranslate">nil</span> if he is not a member of any team.
+       
         #### Example
+       
         Adding the wizard to the 'rogues' team.
+       
         ```lua
         spell.owner.team = "rogues"
         ```
-
+       
         To make this work, don't forget to create the 'rogues' team first:
-        ```
+       
+        ```lua
         /scoreboard teams add rogues
         ```
-
+       
         #### Example
+       
         Printing the wizard's team name.
+       
         ```lua
         print( spell.owner.team)
         ```
-
 functions:
 ---
 
-An instance of the <span class="notranslate">Player</span> class represents a specific player who is currently logged into the world.
+An instance of the <span class="notranslate">Player</span> class represents a specific player who
+is currently logged into the world.
--- src/main/java/net/wizardsoflua/lua/classes/entity/PlayerClass.java
+++ src/main/java/net/wizardsoflua/lua/classes/entity/PlayerClass.java
@@ -11,6 +11,7 @@ import com.google.auto.service.AutoService;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.play.server.SPacketPlayerPosLook.EnumFlags;
 import net.minecraft.scoreboard.Team;
 import net.minecraft.world.GameType;
@@ -29,6 +30,10 @@ import net.wizardsoflua.lua.classes.BasicLuaClass;
 import net.wizardsoflua.lua.classes.LuaClassAttributes;
 import net.wizardsoflua.lua.classes.common.Delegator;
 
+/**
+ * An instance of the <span class="notranslate">Player</span> class represents a specific player who
+ * is currently logged into the world.
+ */
 @AutoService(LuaConverter.class)
 @LuaClassAttributes(name = PlayerClass.NAME, superClass = EntityClass.class)
 @GenerateLuaClassTable(instance = PlayerClass.Instance.class)
@@ -48,16 +53,16 @@ public final class PlayerClass
 
   @Override
   protected Delegator<Instance<EntityPlayerMP>> toLuaInstance(EntityPlayer javaInstance) {
-    return new PlayerClassInstanceTable<>(new Instance<>((EntityPlayerMP) javaInstance, injector), getTable(),
-        converters);
+    return new PlayerClassInstanceTable<>(new Instance<>((EntityPlayerMP) javaInstance, injector),
+        getTable(), converters);
   }
 
   @GenerateLuaInstanceTable
   public static class Instance<D extends EntityPlayerMP> extends EntityLivingBaseClass.Instance<D> {
-    
+
     @Resource
     private WizardsOfLua wol;
-    
+
     public Instance(D delegate, Injector injector) {
       super(delegate, injector);
     }
@@ -66,14 +71,26 @@ public final class PlayerClass
     public String getEntityType() {
       return "player";
     }
-    
+
     @Override
     public void putNbt(Table nbt) {
       throw new LuaRuntimeException(format("%s not supported for class %s", "putNbt", NAME));
     }
 
+    /**
+     * This is this player's game mode. It can be one of 'survival', 'adventure', 'creative',
+     * 'spectator'.
+     *
+     * #### Example
+     *
+     * Setting the gamemode of this spell's owner to 'adventure'.
+     *
+     * <code>
+     * spell.owner.gamemode = "adventure"
+     * </code>
+     */
     @LuaProperty
-    public GameType getGamemode() {         
+    public GameType getGamemode() {
       return delegate.interactionManager.getGameType();
     }
 
@@ -82,11 +99,40 @@ public final class PlayerClass
       delegate.setGameType(gamemode);
     }
 
+    /**
+     * This is <span class="notranslate">true</span> if this player has operator privileges.
+     */
     @LuaProperty
     public boolean isOperator() {
       return wol.getPermissions().hasOperatorPrivileges(delegate.getPersistentID());
     }
-    
+
+    /**
+     * The 'team' is the name of the team this player belongs to, or
+     * <span class="notranslate">nil</span> if he is not a member of any team.
+     *
+     * #### Example
+     *
+     * Adding the wizard to the 'rogues' team.
+     *
+     * <code>
+     * spell.owner.team = "rogues"
+     * </code>
+     *
+     * To make this work, don't forget to create the 'rogues' team first:
+     *
+     * <code>
+     * /scoreboard teams add rogues
+     * </code>
+     *
+     * #### Example
+     *
+     * Printing the wizard's team name.
+     *
+     * <code>
+     * print( spell.owner.team)
+     * </code>
+     */
     @LuaProperty
     public @Nullable String getTeam() {
       Team team = delegate.getTeam();
@@ -138,6 +184,60 @@ public final class PlayerClass
       relativeFlags.remove(EnumFlags.X_ROT);
       relativeFlags.remove(EnumFlags.Y_ROT);
       delegate.connection.setPlayerLocation(0, 0, 0, rotationYaw, rotationPitch, relativeFlags);
+    }
+
+
+    // TODO remove the following part when the lua doc generator is fixed
+
+    /**
+     * The 'health' is the energy of this entity. When it falls to zero this entity dies.
+     */
+    @Override
+    @LuaProperty
+    public float getHealth() {
+      // NOTE: this method is redeclared here to force the lua doc generator to write this docs
+      return super.getHealth();
+    }
+
+    @Override
+    @LuaProperty
+    public void setHealth(float value) {
+      // NOTE: this method is redeclared here to force the lua doc generator to write this docs
+      super.setHealth(value);
+    }
+
+    /**
+     * This is the [item](/modules/Item) this entity is holding in its main hand.
+     */
+    @Override
+    @LuaProperty
+    public @Nullable ItemStack getMainhand() {
+      // NOTE: this method is redeclared here to force the lua doc generator to write this docs
+      return super.getMainhand();
+    }
+
+    @Override
+    @LuaProperty
+    public void setMainhand(@Nullable ItemStack mainhand) {
+      // NOTE: this method is redeclared here to force the lua doc generator to write this docs
+      super.setMainhand(mainhand);
+    }
+
+    /**
+     * This is the [item](/modules/Item) this entity is holding in his off hand.
+     */
+    @Override
+    @LuaProperty
+    public @Nullable ItemStack getOffhand() {
+      // NOTE: this method is redeclared here to force the lua doc generator to write this docs
+      return super.getOffhand();
+    }
+
+    @Override
+    @LuaProperty
+    public void setOffhand(@Nullable ItemStack offhand) {
+      // NOTE: this method is redeclared here to force the lua doc generator to write this docs
+      super.setOffhand(offhand);
     }
   }
 }
