55163b02891eb7a951c1ecf9a19c2ab71e843ab3
Michael Karneim <mkarneim@googlemail.com>
2019-05-03 18:55:56 +0200

    Update docs for Time

--- src/main/java/net/wizardsoflua/lua/module/time/TimeModule.java
+++ src/main/java/net/wizardsoflua/lua/module/time/TimeModule.java
@@ -28,7 +28,8 @@ import net.wizardsoflua.extension.spell.spi.SpellExtension;
 import net.wizardsoflua.lua.extension.LuaTableExtension;
 
 /**
- * The Time module provides access to time related properties of the active Spell's world.
+ * The <span class="notranslate">Time</span> module provides access to time related properties of
+ * the active Spell's world.
  */
 @AutoService(SpellExtension.class)
 @GenerateLuaModuleTable
@@ -61,8 +62,8 @@ public class TimeModule extends LuaTableExtension {
   }
 
   /**
-   * The allowance is the number of lua ticks that are left before the active spell must sleep for
-   * at least one game tick.
+   * The allowance is the number of lua ticks that are left before the spell or event listener is
+   * broken or sent to sleep, depending on [autosleep](#autosleep).
    */
   @LuaProperty
   public long getAllowance() {
@@ -71,8 +72,11 @@ public class TimeModule extends LuaTableExtension {
 
   /**
    * The autosleep value defines whether the current spell should go to sleep automatically when its
-   * allowance is exceeded. If this is set to false, the spell will never go to sleep automatically,
-   * but instead will be broken when its allowance falls below zero. Default is true.
+   * allowance is exceeded.
+   *
+   * If this is set to false, the spell will never go to sleep automatically, but instead will be
+   * broken when its allowance reaches zero. Default is true normally, but in an [event
+   * interceptor](/modules/Events#intercept) 'autosleep' is always false and can't be changed.
    */
   @LuaProperty
   public boolean isAutosleep() {
@@ -94,7 +98,7 @@ public class TimeModule extends LuaTableExtension {
 
   /**
    * The luatime is the number of lua ticks that the current spell has worked since it has been
-   * casted.
+   * casted. This includes lua ticks of event listeners.
    */
   @LuaProperty
   public long getLuatime() {
@@ -112,6 +116,23 @@ public class TimeModule extends LuaTableExtension {
   /**
    * Returns a string with the current real date and time. If you want you can change the format by
    * providing an optional format string.
+   *
+   * #### Example
+   *
+   * Printing the current date and time in ISO date-time format, such as '2011-12-03T10:15:30'.
+   *
+   * <code>
+   * print(Time.getDate())
+   * </code>
+   *
+   * #### Example
+   *
+   * Printing the current date and time with a custom format, such as '03 Oct, 2017 - 10:15:29'.
+   *
+   * <code>
+   * print(Time.getDate("dd MMM, yyyy - HH:mm:ss"))
+   * </code>
+   *
    */
   @LuaFunction
   public String getDate(@Nullable String pattern) {
@@ -139,7 +160,30 @@ public class TimeModule extends LuaTableExtension {
   }
 
   /**
-   * Forces the current spell to sleep for the given amount of game ticks.
+   * Forces the current spell to sleep for the given number of game ticks.
+   *
+   * If the number is 0, the spell won't sleep. If the number is negative, this function will issue
+   * an error. If the number is nil, the spell might go to sleep or not. This depends on the number
+   * of lua ticks that are already consumed by this spell.
+   *
+   * The rule is as follows: the spell will be sent to sleep if the spell's allowance falls below
+   * the half value of the spell's initial allowance.
+   *
+   * #### Example
+   *
+   * Sending the current spell to sleep for 100 game ticks, which are approximately 5 seconds.
+   *
+   * <code>
+   * Time.sleep(100)
+   * </code>
+   *
+   * Since <span class="notranslate">'sleep'</span> is a widely used function, there is a shortcut
+   * for it.
+   *
+   * <code>
+   * sleep(100)
+   * </code>
+   *
    */
   @LuaFunction(name = SleepFunction.NAME)
   @LuaFunctionDoc(args = {"ticks"}, returnType = LuaTypes.NIL)
