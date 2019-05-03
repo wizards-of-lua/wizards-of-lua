654dcc412fd14116b61653757f1568256bbe7263
Michael Karneim <mkarneim@googlemail.com>
2019-05-03 15:17:56 +0200

    Update docs for Entity

--- docs/_modules/Entity/motion.md
+++
@@ -1,5 +0,0 @@
-#### Example
-Pushing the wizard up into the sky.
-```lua
-spell.owner.motion=Vec3(0,5,0)
-```
--- src/main/java/net/wizardsoflua/lua/classes/entity/EntityClass.java
+++ src/main/java/net/wizardsoflua/lua/classes/entity/EntityClass.java
@@ -49,9 +49,11 @@ import net.wizardsoflua.lua.nbt.NbtConverter;
 @GenerateLuaClassTable(instance = EntityClass.Instance.class)
 @GenerateLuaDoc(subtitle = "The Base Class of all Organic or Inorganic Entities")
 public final class EntityClass extends BasicLuaClass<Entity, EntityClass.Instance<Entity>> {
+
   public static final String NAME = "Entity";
   @Resource
   private LuaConverters converters;
+
   @Resource
   private Injector injector;
 
@@ -68,6 +70,7 @@ public final class EntityClass extends BasicLuaClass<Entity, EntityClass.Instanc
 
   @GenerateLuaInstanceTable
   public static class Instance<D extends Entity> extends LuaInstance<D> {
+
     @Resource
     private LuaConverters converters;
     @Inject
@@ -79,11 +82,37 @@ public final class EntityClass extends BasicLuaClass<Entity, EntityClass.Instanc
     }
 
     /**
-     * This is true, if this entity is alive, false otherwise.
+     * The 'addTag' function adds the given tag to the set of [tags](/modules/Entity/#tags) of this
+     * entity. This function returns true if the tag was added successfully.
      */
-    @LuaProperty
-    public boolean isAlive() {
-      return !delegate.isDead;
+    @LuaFunction
+    public boolean addTag(String tag) {
+      return delegate.addTag(tag);
+    }
+
+    /**
+     * The 'dropItem' function drops the given item at this entity's position modified by the
+     * optionally given vertical offset.
+     *
+     * #### Example
+     *
+     * Dropping the block at the spell's position as an item.
+     *
+     * <code>
+     * if spell.block.name ~= "air" then
+     *   local item = spell.block:asItem()
+     *   spell:dropItem( item)
+     *   spell.block = Blocks.get("air")
+     * end
+     * </code>
+     */
+    @LuaFunction
+    public EntityItem dropItem(ItemStack item, @Nullable Float offsetY) {
+      offsetY = ofNullable(offsetY).orElse(0f);
+      if (item.getCount() == 0) {
+        throw new IllegalArgumentException("Can't drop an item with count==0");
+      }
+      return delegate.entityDropItem(item, offsetY);
     }
 
     /**
@@ -129,16 +158,30 @@ public final class EntityClass extends BasicLuaClass<Entity, EntityClass.Instanc
     }
 
     /**
-     * The 'invisible' property is true if this entity can not be seen by others.
-     */
-    @LuaProperty
-    public boolean isInvisible() {
-      return delegate.isInvisible();
-    }
-
-    /**
      * The 'lookVec' is a 3-dimensional vector that points into the direction this entity is looking
      * at, or nil, if it is not looking anywhere, for example, if it has no eyes.
+     *
+     * #### Example
+     *
+     * Letting the wizard spit 10 meters into the direction he is looking at.
+     *
+     * <code>
+     * local dir=spell.owner.lookVec
+     * local s=spell.owner.pos+Vec3(0,spell.owner.eyeHeight,0)
+     * for i=1,10,0.1 do
+     *   spell.pos=s+dir*i
+     *   spell:execute("particle droplet ~ ~ ~ 0 0 0 0")
+     * end
+     * </code>
+     *
+     * #### Example
+     *
+     * Moving the spell into the owners eye and pointing it into the owner's look direction.
+     *
+     * <code>
+     * spell.pos = spell.owner.pos+Vec3(0,spell.owner.eyeHeight,0);
+     * spell.lookVec = spell.owner.lookVec
+     * </code>
      */
     @LuaProperty
     public @Nullable Vec3d getLookVec() {
@@ -155,6 +198,14 @@ public final class EntityClass extends BasicLuaClass<Entity, EntityClass.Instanc
     /**
      * The 'motion' is a 3-dimensional vector that represents the velocity of this entity when it is
      * moved by some external force, e.g. when it is falling or when it is pushed by an explosion.
+     *
+     * #### Example
+     *
+     * Pushing the wizard up into the sky.
+     *
+     * <code>
+     * spell.owner.motion=Vec3(0,5,0)
+     * </code>
      */
     @LuaProperty
     public Vec3d getMotion() {
@@ -203,10 +254,24 @@ public final class EntityClass extends BasicLuaClass<Entity, EntityClass.Instanc
 
     /**
      * The 'nbt' value (short for Named Binary Tag) is a table of entity-specifc key-value pairs
-     * also called [data tags](https://minecraft.gamepedia.com/Commands#Data_tags). The nbt property
-     * is readonly but gives you a modifiable copy of the internal value. You can change the
-     * contents, but to activate them you have to assign the modified table to the entity by using
-     * the [putNbt()](/modules/Entity/#putNbt) function.
+     * also called [data tags](https://minecraft.gamepedia.com/Commands#Data_tags).
+     *
+     * The nbt property is readonly but gives you a modifiable copy of the internal value.
+     *
+     * You can change the contents, but to activate them you have to assign the modified table to
+     * the entity by using the [putNbt()](/modules/Entity/#putNbt) function.
+     *
+     * #### Example
+     *
+     * Putting on a helmet on all zombies.
+     *
+     * <code>
+     * for _,zombie in pairs(Entities.find("@e[type=zombie]")) do
+     *   local n=zombie.nbt
+     *   n.ArmorItems[4]={Count=1,id="iron_helmet"}
+     *   zombie:putNbt(n)
+     * end
+     * </code>
      */
     @LuaProperty
     public NBTTagCompound getNbt() {
@@ -234,22 +299,6 @@ public final class EntityClass extends BasicLuaClass<Entity, EntityClass.Instanc
     }
 
     /**
-     * The 'rotationYaw' is the rotation of this entity around its Y axis in degrees. For example, a
-     * value of 0 means the entity is facing south. 90 corresponds to west, and 45 to south-west.
-     */
-    @LuaProperty
-    public float getRotationYaw() {
-      return delegate.rotationYaw;
-    }
-
-    @LuaProperty
-    public void setRotationYaw(float rotationYaw) {
-      rotationYaw = MathHelper.wrapDegrees(rotationYaw);
-      delegate.rotationYaw = rotationYaw;
-      delegate.setRotationYawHead(rotationYaw);
-    }
-
-    /**
      * The 'rotationPitch' is the rotation of this entity's head around its X axis in degrees. A
      * value of -90 means the entity is looking straight up. A value of 90 means it is looking
      * straight down.
@@ -272,23 +321,40 @@ public final class EntityClass extends BasicLuaClass<Entity, EntityClass.Instanc
     }
 
     /**
-     * This is true, if this entity is currently sneaking, false otherwise.
+     * The 'rotationYaw' is the rotation of this entity around its Y axis in degrees. For example, a
+     * value of 0 means the entity is facing south. 90 corresponds to west, and 45 to south-west.
      */
     @LuaProperty
-    public boolean isSneaking() {
-      return delegate.isSneaking();
+    public float getRotationYaw() {
+      return delegate.rotationYaw;
     }
 
-    /**
-     * The 'sprinting' property is true whenever this entity is running fast.
-     */
     @LuaProperty
-    public boolean isSprinting() {
-      return delegate.isSprinting();
+    public void setRotationYaw(float rotationYaw) {
+      rotationYaw = MathHelper.wrapDegrees(rotationYaw);
+      delegate.rotationYaw = rotationYaw;
+      delegate.setRotationYawHead(rotationYaw);
     }
 
     /**
      * The 'tags' value is a list of strings that have been assigned to this entity.
+     *
+     * #### Example
+     *
+     * Tagging the wizard as great and fearsome.
+     *
+     * <code>
+     * spell.owner.tags = {"great", "fearsome"}
+     * </code>
+     *
+     * #### Example
+     *
+     * Printing the wizard's tags.
+     *
+     * <code>
+     * print( str( spell.owner.tags))
+     * </code>
+     *
      */
     @LuaProperty
     public Collection<String> getTags() {
@@ -327,29 +393,54 @@ public final class EntityClass extends BasicLuaClass<Entity, EntityClass.Instanc
     }
 
     /**
-     * The 'addTag' function adds the given tag to the set of [tags](/modules/Entity/#tags) of this
-     * entity. This function returns true if the tag was added successfully.
+     * This is true, if this entity is alive, false otherwise.
      */
-    @LuaFunction
-    public boolean addTag(String tag) {
-      return delegate.addTag(tag);
+    @LuaProperty
+    public boolean isAlive() {
+      return !delegate.isDead;
     }
 
     /**
-     * The 'dropItem' function drops the given item at this entity's position modified by the
-     * optionally given vertical offset.
+     * The 'invisible' property is true if this entity can not be seen by others.
      */
-    @LuaFunction
-    public EntityItem dropItem(ItemStack item, @Nullable Float offsetY) {
-      offsetY = ofNullable(offsetY).orElse(0f);
-      if (item.getCount() == 0) {
-        throw new IllegalArgumentException("Can't drop an item with count==0");
-      }
-      return delegate.entityDropItem(item, offsetY);
+    @LuaProperty
+    public boolean isInvisible() {
+      return delegate.isInvisible();
+    }
+
+    /**
+     * This is true, if this entity is currently sneaking, false otherwise.
+     */
+    @LuaProperty
+    public boolean isSneaking() {
+      return delegate.isSneaking();
+    }
+
+    /**
+     * The 'sprinting' property is true whenever this entity is running fast.
+     */
+    @LuaProperty
+    public boolean isSprinting() {
+      return delegate.isSprinting();
     }
 
     /**
      * The 'kill' function kills this entity during the next game tick.
+     *
+     * #### Example
+     *
+     * Killing all pigs that are swimming in liquid material.
+     *
+     * <code>
+     * local pigs = Entities.find("@e[type=pig]")
+     * for _,pig in pairs(pigs) do
+     *   spell.pos = pig.pos
+     *   if spell.block.material.liquid then
+     *     pig:kill()
+     *   end
+     * end
+     * </code>
+     *
      */
     @LuaFunction
     public void kill() {
@@ -358,11 +449,22 @@ public final class EntityClass extends BasicLuaClass<Entity, EntityClass.Instanc
 
     /**
      * The 'move' function teleports this entity instantly to the position relative to its current
-     * position specified by the given direction and distance. If no distance is specified, 1 meter
-     * is taken as default distance. Valid direction values are absolute directions ('up', 'down',
-     * 'north', 'east', 'south', and 'west'), as well as relative directions ('forward', 'back',
-     * 'left', and 'right'). Relative directions are interpreted relative to the direction the
-     * entity is [facing](/modules/Entity/#facing).
+     * position specified by the given direction and distance.
+     *
+     * If no distance is specified, 1 meter is taken as default distance. Valid direction values are
+     * absolute directions ('up', 'down', 'north', 'east', 'south', and 'west'), as well as relative
+     * directions ('forward', 'back', 'left', and 'right').
+     *
+     * Relative directions are interpreted relative to the direction the entity is
+     * [facing](/modules/Entity/#facing).
+     *
+     * #### Example
+     *
+     * Moving the spell's owner for half a meter to the left.
+     *
+     * <code>
+     * spell.owner:move( "left", 0.5)
+     * </code>
      */
     @LuaFunction
     public void move(String directionName, @Nullable Double distance) {
@@ -380,8 +482,32 @@ public final class EntityClass extends BasicLuaClass<Entity, EntityClass.Instanc
 
     /**
      * The 'putNbt' function inserts the given table entries into the [nbt](/modules/Entity/#nbt)
-     * property of this entity. Please note that this function is not supported for
-     * [Player](/modules/Player/) objects.
+     * property of this entity.
+     *
+     * Please note that this function is not supported for [Player](/modules/Player/) objects.
+     *
+     * #### Example
+     *
+     * Cutting the health of all bats to half.
+     *
+     * <code>
+     * local e = Entities.find("@e[type=bat]")
+     * for _,bat in pairs(e) do
+     *   local h = math.floor(bat.nbt.Health/2)
+     *   bat:putNbt({Health=h})
+     *   print(bat.nbt.Health)
+     * end
+     * </code>
+     *
+     * #### Example
+     *
+     * Finding all pigs and putting a saddle on each of them.
+     *
+     * <code>
+     * for _,pig in pairs(Entities.find("@e[type=pig]")) do
+     *   pig:putNbt({Saddle=1})
+     * end
+     * </code>
      */
     @LuaFunction
     public void putNbt(Table nbt) {
@@ -401,9 +527,24 @@ public final class EntityClass extends BasicLuaClass<Entity, EntityClass.Instanc
     }
 
     /**
-     * The 'scanView' function scans the view of this entity for the next (non-liquid) block. On
-     * success it returns a [BlockHit](/modules/BlockHit/), otherwise nil. It scans the view with a
-     * line-of-sight-range of up to the given distance (meter).
+     * The 'scanView' function scans the view of this entity for the next (non-liquid) block.
+     *
+     * On success it returns a [BlockHit](/modules/BlockHit/), otherwise nil. It scans the view with
+     * a line-of-sight-range of up to the given distance (meter).
+     *
+     * #### Example
+     *
+     * Prints the name of the block the spell's owner is looking at (up to a maximum distance of 10
+     * meters).
+     *
+     * <code>
+     * local maxDistance = 10
+     * local hit = spell.owner:scanView( maxDistance)
+     * if hit then
+     *   spell.pos = hit.pos
+     *   print(spell.owner.name.." is looking at "..spell.block.name)
+     * end
+     * </code>
      */
     @LuaFunction
     public RayTraceResult scanView(float distance) {
@@ -411,5 +552,7 @@ public final class EntityClass extends BasicLuaClass<Entity, EntityClass.Instanc
       Vec3d end = start.add(delegate.getLookVec().scale(distance));
       return delegate.getEntityWorld().rayTraceBlocks(start, end, false);
     }
+
   }
+
 }
