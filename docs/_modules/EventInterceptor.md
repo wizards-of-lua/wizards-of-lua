c84d3976c38bd7a996175ac2369f48b8ba2dd22e
Michael Karneim <mkarneim@googlemail.com>
2019-05-03 16:03:58 +0200

    Update docs of EventInterceptor

--- docs/_modules/EventInterceptor.md
+++ docs/_modules/EventInterceptor.md
@@ -1,24 +1,36 @@
 ---
 name: EventInterceptor
 title: EventInterceptor
-subtitle:
+subtitle: Intercepting Events
 type: class
+extends: Object
 layout: module
 properties:
 functions:
   - name: stop
-    results: nil
+    parameters: 
+    results: 'nil'
     description: |
-        The 'stop' function terminates this interceptor so that the corresponding function is no longer called for new events.
+        The 'stop' function terminates this interceptor so that the corresponding function is no
+        longer called for new events.
+       
         #### Example
+       
         Intercepting the next chat event and stopping immediately once the first one occurs.
+       
         ```lua
         local i
         i = Events.on('ChatEvent'):call(function(event)
-            print(str(event))
-            i:unsubscribe()
+          print(event.player.name, event.message)
+          i:unsubscribe()
         end)
         ```
 ---
 
-An <span class="notranslate">EventInterceptor</span> represents an interceptor of events. A interceptor can be obtained through [Events.intercept()](/modules/Events#intercept).
+An <span class="notranslate">EventInterceptor</span> represents an interceptor of events.
+
+An interceptor can be obtained through [Events.intercept()](/modules/Events#intercept) and
+[Events.on(...):call()](/modules/Events#on).
+
+In contrast to an [EventQueue](/modules/EventQueue), an event interceptor is capable of event
+mutation and event cancellation since it is called 'in-line' with the event occurrence.
--- src/main/java/net/wizardsoflua/lua/classes/eventinterceptor/EventInterceptorClass.java
+++ src/main/java/net/wizardsoflua/lua/classes/eventinterceptor/EventInterceptorClass.java
@@ -1,6 +1,7 @@
 package net.wizardsoflua.lua.classes.eventinterceptor;
 
 import com.google.auto.service.AutoService;
+
 import net.sandius.rembulan.Table;
 import net.wizardsoflua.annotation.GenerateLuaClassTable;
 import net.wizardsoflua.annotation.GenerateLuaDoc;
@@ -14,11 +15,21 @@ import net.wizardsoflua.lua.classes.LuaClassAttributes;
 import net.wizardsoflua.lua.classes.LuaInstance;
 import net.wizardsoflua.lua.classes.common.Delegator;
 
+/**
+ * An <span class="notranslate">EventInterceptor</span> represents an interceptor of events.
+ *
+ * An interceptor can be obtained through [Events.intercept()](/modules/Events#intercept) and
+ * [Events.on(...):call()](/modules/Events#on).
+ *
+ * In contrast to an [EventQueue](/modules/EventQueue), an event interceptor is capable of event
+ * mutation and event cancellation since it is called 'in-line' with the event occurrence.
+ */
 @AutoService(LuaConverter.class)
 @LuaClassAttributes(name = EventInterceptorClass.NAME)
 @GenerateLuaClassTable(instance = EventInterceptorClass.Instance.class)
-@GenerateLuaDoc
-public final class EventInterceptorClass extends BasicLuaClass<EventInterceptor, EventInterceptorClass.Instance<EventInterceptor>> {
+@GenerateLuaDoc(subtitle = "Intercepting Events")
+public final class EventInterceptorClass
+    extends BasicLuaClass<EventInterceptor, EventInterceptorClass.Instance<EventInterceptor>> {
   public static final String NAME = "EventInterceptor";
   @Resource
   private LuaConverters converters;
@@ -40,6 +51,22 @@ public final class EventInterceptorClass extends BasicLuaClass<EventInterceptor,
       super(delegate);
     }
 
+    /**
+     * The 'stop' function terminates this interceptor so that the corresponding function is no
+     * longer called for new events.
+     *
+     * #### Example
+     *
+     * Intercepting the next chat event and stopping immediately once the first one occurs.
+     *
+     * <code>
+     * local i
+     * i = Events.on('ChatEvent'):call(function(event)
+     *   print(event.player.name, event.message)
+     *   i:unsubscribe()
+     * end)
+     * </code>
+     */
     @LuaFunction
     public void stop() {
       delegate.stop();
