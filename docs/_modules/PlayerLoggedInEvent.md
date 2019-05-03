ca1fa1628dff62ee429e1ea0529594f3757b5641
Michael Karneim <mkarneim@googlemail.com>
2019-05-03 18:23:33 +0200

    Update docs for PlayerLoggedInEvent

--- docs/_modules/PlayerLoggedInEvent.md
+++ docs/_modules/PlayerLoggedInEvent.md
@@ -5,14 +5,14 @@ subtitle: When a Player Joins the World
 type: event
 extends: Event
 layout: module
-properties:  
-  - name: player
-    type: "[Player](/modules/Player/)"
+properties:
+  - name: 'player'
+    type: '[Player](/modules/Player)'
     access: r
-    description: "The player that joined the world.
-    "
+    description: |
+        The player who joined the world.
 functions:
 ---
 
-The <span class="notranslate">PlayerLoggedInEvent</span> is fired whenever a [Player](/modules/Player) joins the
-world (server).
+The <span class="notranslate">PlayerLoggedInEvent</span> is fired whenever a
+[Player](/modules/Player) joins the world (server).
--- src/main/java/net/wizardsoflua/lua/classes/event/PlayerLoggedInEventClass.java
+++ src/main/java/net/wizardsoflua/lua/classes/event/PlayerLoggedInEventClass.java
@@ -1,6 +1,7 @@
 package net.wizardsoflua.lua.classes.event;
 
 import com.google.auto.service.AutoService;
+
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.sandius.rembulan.Table;
@@ -16,11 +17,16 @@ import net.wizardsoflua.lua.classes.BasicLuaClass;
 import net.wizardsoflua.lua.classes.LuaClassAttributes;
 import net.wizardsoflua.lua.classes.common.Delegator;
 
+/**
+ * The <span class="notranslate">PlayerLoggedInEvent</span> is fired whenever a
+ * [Player](/modules/Player) joins the world (server).
+ */
 @AutoService(LuaConverter.class)
 @LuaClassAttributes(name = PlayerLoggedInEventClass.NAME, superClass = EventClass.class)
 @GenerateLuaClassTable(instance = PlayerLoggedInEventClass.Instance.class)
-@GenerateLuaDoc(type = EventClass.TYPE)
-public final class PlayerLoggedInEventClass extends BasicLuaClass<PlayerEvent.PlayerLoggedInEvent, PlayerLoggedInEventClass.Instance<PlayerEvent.PlayerLoggedInEvent>> {
+@GenerateLuaDoc(type = EventClass.TYPE, subtitle = "When a Player Joins the World")
+public final class PlayerLoggedInEventClass extends
+    BasicLuaClass<PlayerEvent.PlayerLoggedInEvent, PlayerLoggedInEventClass.Instance<PlayerEvent.PlayerLoggedInEvent>> {
   public static final String NAME = "PlayerLoggedInEvent";
   @Resource
   private LuaConverters converters;
@@ -46,6 +52,9 @@ public final class PlayerLoggedInEventClass extends BasicLuaClass<PlayerEvent.Pl
       super(delegate, name, injector);
     }
 
+    /**
+     * The player who joined the world.
+     */
     @LuaProperty
     public EntityPlayer getPlayer() {
       return delegate.player;
