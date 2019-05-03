8836ae05383889e8745cf7cd8fa95fd9946b5635
Michael Karneim <mkarneim@googlemail.com>
2019-05-03 17:29:39 +0200

    Update docs for Item

--- src/main/java/net/wizardsoflua/lua/classes/item/ItemClass.java
+++ src/main/java/net/wizardsoflua/lua/classes/item/ItemClass.java
@@ -24,6 +24,10 @@ import net.wizardsoflua.lua.classes.LuaInstance;
 import net.wizardsoflua.lua.classes.common.Delegator;
 import net.wizardsoflua.lua.nbt.NbtConverter;
 
+/**
+ * The <span class="notranslate">Item</span> class represents all things a creature can hold in its
+ * hands and which can be stored in an inventory.
+ */
 @AutoService(LuaConverter.class)
 @LuaClassAttributes(name = ItemClass.NAME)
 @GenerateLuaClassTable(instance = ItemClass.Instance.class)
@@ -58,6 +62,9 @@ public final class ItemClass extends BasicLuaClass<ItemStack, ItemClass.Instance
       injector.injectMembers(this);
     }
 
+    /**
+     * When this item is stackable, this is the number of items stacked.
+     */
     @LuaProperty
     public int getCount() {
       return delegate.getCount();
@@ -68,6 +75,13 @@ public final class ItemClass extends BasicLuaClass<ItemStack, ItemClass.Instance
       delegate.setCount(count);
     }
 
+    /**
+     * This is the numerical damage value of this item.
+     *
+     * The higher the value, the more damaged this item is.
+     *
+     * A value of 0 means the item is not damaged.
+     */
     @LuaProperty
     public int getDamage() {
       return delegate.getItemDamage();
@@ -78,6 +92,9 @@ public final class ItemClass extends BasicLuaClass<ItemStack, ItemClass.Instance
       delegate.setItemDamage(meta);
     }
 
+    /**
+     * This is the name of the item.
+     */
     @LuaProperty
     public String getDisplayName() {
       return delegate.getDisplayName();
@@ -88,6 +105,9 @@ public final class ItemClass extends BasicLuaClass<ItemStack, ItemClass.Instance
       delegate.setStackDisplayName(displayName);
     }
 
+    /**
+     * The 'id' indentifies the type of this item.
+     */
     @LuaProperty
     public String getId() {
       ResourceLocation name = delegate.getItem().getRegistryName();
@@ -98,11 +118,22 @@ public final class ItemClass extends BasicLuaClass<ItemStack, ItemClass.Instance
       }
     }
 
+    /**
+     * The 'nbt' value (short for Named Binary Tag) is a table of [item-specifc key-value
+     * pairs](https://minecraft.gamepedia.com/Player.dat_format#Item_structure).
+     */
     @LuaProperty
     public NBTTagCompound getNbt() {
       return delegate.serializeNBT();
     }
 
+    /**
+     * This is the number of enchantment levels to add to the base level cost when repairing,
+     * combining, or renaming this item with an anvil.
+     *
+     * If this value is negative, it will effectively lower the cost. However, the total cost will
+     * never fall below zero.
+     */
     @LuaProperty
     public int getRepairCost() {
       return delegate.getRepairCost();
@@ -113,6 +144,22 @@ public final class ItemClass extends BasicLuaClass<ItemStack, ItemClass.Instance
       delegate.setRepairCost(repairCost);
     }
 
+    /**
+     * The 'putNbt' function inserts the given table entries into this item's
+     * [nbt](/modules/Item/#nbt) property.
+     *
+     * #### Example
+     *
+     * Setting the lore (description) of the item in the wizard's hand.
+     *
+     * <code>
+     * local text1 = "Very important magical stuff."
+     * local text2 = "Use with caution!"
+     * local loreTbl = { text1, text2}
+     * local item = spell.owner.mainhand
+     * item:putNbt({tag={display={Lore=loreTbl}}})
+     * </code>
+     */
     @LuaFunction
     public void putNbt(Table nbt) {
       NBTTagCompound oldNbt = delegate.serializeNBT();
