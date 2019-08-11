a10b79ad863d49bd7c7de49e8027bece0208e84e
Adrodoc <adrodoc55@googlemail.com>
2019-08-11 14:27:22 +0200

    Fix reflective access to obfuscated code

--- src/main/java/net/wizardsoflua/CommandSourceUtils.java
+++ src/main/java/net/wizardsoflua/CommandSourceUtils.java
@@ -1,36 +1,16 @@
 package net.wizardsoflua;
 
 import static net.minecraft.util.text.TextFormatting.RED;
-import java.lang.reflect.Field;
-import javax.annotation.Nullable;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.ICommandSource;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 public class CommandSourceUtils {
-  private static @Nullable Field sourceField;
-
-  private static Field getSourceField() {
-    if (sourceField == null) {
-      try {
-        sourceField = CommandSource.class.getDeclaredField("source");
-        sourceField.setAccessible(true);
-      } catch (NoSuchFieldException e) {
-        throw new RuntimeException(e);
-      }
-    }
-    return sourceField;
-  }
-
   private static ICommandSource getICommandSource(CommandSource source) {
-    Field field = getSourceField();
-    try {
-      return (ICommandSource) field.get(source);
-    } catch (IllegalAccessException e) {
-      throw new RuntimeException(e);
-    }
+    return ObfuscationReflectionHelper.getPrivateValue(CommandSource.class, source, "source");
   }
 
   public static void sendAndLogFeedback(CommandSource source, ITextComponent message) {
