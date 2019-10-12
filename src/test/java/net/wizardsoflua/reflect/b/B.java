396efbb82efd0e2635280ae06a04149a9dee42fa
Adrodoc <adrodoc55@googlemail.com>
2019-10-12 19:42:21 +0200

    Add comment on unsuppressable warning

--- src/test/java/net/wizardsoflua/reflect/b/B.java
+++ src/test/java/net/wizardsoflua/reflect/b/B.java
@@ -3,6 +3,7 @@ package net.wizardsoflua.reflect.b;
 import net.wizardsoflua.reflect.a.A;
 
 public class B extends A {
+  // This method is used by ReflectionUtils_isOverridden_Test
   boolean m() {
     return true;
   }
